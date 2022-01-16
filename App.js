e645039b39e5256112a49c2459ed9949cb6d3575
Srishi__1803 <65821403+ss-shrishi2000@users.noreply.github.com>
2022-01-16 12:27:59 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import React from "react";
+import './App.css';
+import Pathfind from './components/Pathfind';
+
+
+const App = () => {
+    return (
+        <div>
+            <Pathfind />
+        </div>
+    );
+};
+
+export default App;
